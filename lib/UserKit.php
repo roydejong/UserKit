1703420500ff8f785936e7fff1911eb959003231
Roy de Jong <email@roydejong.net>
2017-02-12 17:07:47 +0100

    Project: Init psr-4 autoloader, create test class

---
+++ lib/UserKit.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace UserKit;
+
+/**
+ * Main API for the UserKit analytics library.
+ */
+class UserKit
+{
+    /**
+     * @return bool
+     */
+    public function test(): bool
+    {
+        return true;
+    }
+}
