678dcdb96b64c5870a4d8364d208fd44dfcde24e
Roy de Jong <email@roydejong.net>
2017-02-18 23:53:44 +0100

    WebUI: Layout stub / gcharts stub stuff

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,9 @@
 var UserKit = {
     bootstrap: function () {
-        UserKitLoader.loadCss('lib/bootstrap/bootstrap.min.css');
-        UserKitLoader.loadCss('style/userkit.css');
+        UserKitLoader.loadCss('lib/bootstrap/bootstrap.min.css', function () {
+            UserKitLoader.loadCss('style/userkit.css');
+        });
+
         UserKitLoader.allReady(function () {
             UserKit.pageReady();
         });
@@ -9,6 +11,42 @@ var UserKit = {
 
     pageReady: function () {
         $('#loader').hide();
+        $('#main').fadeIn('fast');
+
+        // Load the Visualization API and the corechart package.
+        google.charts.load('current', {'packages': ['corechart']});
+
+        // Set a callback to run when the Google Visualization API is loaded.
+        google.charts.setOnLoadCallback(drawChart);
+
+        // Callback that creates and populates a data table,
+        // instantiates the pie chart, passes in the data and
+        // draws it.
+        function drawChart() {
+
+            // Create the data table.
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Topping');
+            data.addColumn('number', 'Slices');
+            data.addRows([
+                ['Mushrooms', 3],
+                ['Onions', 1],
+                ['Olives', 1],
+                ['Zucchini', 1],
+                ['Pepperoni', 2]
+            ]);
+
+            // Set chart options
+            var options = {
+                'title': 'How Much Pizza I Ate Last Night',
+                'width': 400,
+                'height': 300
+            };
+
+            // Instantiate and draw our chart, passing in some options.
+            var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+            chart.draw(data, options);
+        }
     }
 };
 
